c1a244905814b919a659b9d3354244c7f8e47a36
Jacob Dawson <jacob.dawson@me.com>
2023-04-06 20:38:19 -0400

    put main into its own function

--- script.py
+++ script.py
@@ -130,12 +130,6 @@ def is_in_val_zone(location, val_location, val_zone_size):
     )
     return tf.get_static_value(tf.logical_and(x_match, y_match))
 
-def is_proper_train_location(location):
-    return not (
-        is_in_val_zone(location, val_location=val_location, val_zone_size=val_zone_size)
-        and is_in_mask_train(location)
-    )
-
 def extract_subvolume(location, volume):
     x = location[0]
     y = location[1]
@@ -153,12 +147,6 @@ def extract_labels(location, labels):
     label = tf.expand_dims(label, axis=-1)
     return label
 
-
-def extract_subvolume_and_label(location):
-    subvolume = extract_subvolume(location, volume)
-    label = extract_labels(location, labels)
-    return subvolume, label
-
 def trivial_baseline(dataset):
     total = 0
     matches = 0.0
@@ -167,209 +155,221 @@ def trivial_baseline(dataset):
         total += tf.cast(tf.reduce_prod(tf.shape(batch_label)), dtype="float32")
     return tf.cast(1.0 - (matches / total), dtype="float32")
 
-def augment_train_data(data, label):
-    data = tf.cast(augmenter(tf.cast(data, dtype="float32")), dtype="float32")
-    return data, label
-
-
-mask = load_mask(split="train", index=1)
-labels = load_labels(split="train", index=1)
-
-mask_test_a = load_mask(split="test", index="a")
-mask_test_b = load_mask(split="test", index="b")
-
-mask_train_1 = load_mask(split="train", index=1)
-labels_train_1 = load_labels(split="train", index=1)
-
-mask_train_2 = load_mask(split="train", index=2)
-labels_train_2 = load_labels(split="train", index=2)
-
-mask_train_3 = load_mask(split="train", index=3)
-labels_train_3 = load_labels(split="train", index=3)
-
-gc.collect()
-
-volume = load_volume(split="train", index=1)
-gc.collect()
-volume = tf.concat([volume, load_volume(split="train", index=2)], axis=1)
-gc.collect()
-volume = tf.concat([volume, load_volume(split="train", index=3)], axis=1)
-# so what has happened now is that we have a 2D image containing our ENTIRE
-# train set, shaped [4000,8417,20] (that is, a 2D image 20 layers deep!), which
-# contains pages 1, 2, and 3. Similarly, our labels and mask (set below) will
-# contain all three pages as well!
-# note that the size listed above depends on SHARED_HEIGHT, which I might mess
-# with because my computer is getting OOM errors
-print(f"total volume: {volume.shape}, {volume.dtype}")
-print(
-    f"Volume's max: {tf.math.reduce_max(volume)}, min: {tf.math.reduce_min(volume)},mean: {tf.math.reduce_mean(volume)}\n"
-)
-
-gc.collect()
-
-labels = tf.concat([labels_train_1, labels_train_2, labels_train_3], axis=1)
-print(f"labels: {labels.shape}, {labels.dtype}")
-
-mask = tf.concat([mask_train_1, mask_train_2, mask_train_3], axis=1)
-print(f"mask: {mask.shape}, {mask.dtype}")
-
-# Free up memory
-del labels_train_1
-del labels_train_2
-del labels_train_3
-del mask_train_1
-del mask_train_2
-del mask_train_3
-
-# val_location = (1300, 1000)
-# val_zone_size = (600, 2000)
-# gotta change these to match our variable height:
-val_location = (
-    int((1300 / 4000) * SHARED_HEIGHT),
-    int((1300 / 8417) * volume.shape[1]),
-)
-val_zone_size = (
-    int((600 / 4000) * SHARED_HEIGHT),
-    int((2000 / 8417) * volume.shape[1]),
-)
-print(val_location, val_zone_size)
-
-
-sample_random_location_train = lambda x: sample_random_location(mask.shape)
-is_in_mask_train = lambda x: is_in_masked_zone(x, mask)
-
-
-train_locations_ds = (
-    tf.data.Dataset.from_tensor_slices([0])
-    .repeat(steps_per_epoch)
-    .map(sample_random_location_train, num_parallel_calls=tf.data.AUTOTUNE)
-)
-train_locations_ds = train_locations_ds.filter(is_proper_train_location)
-
-
-shuffle_buffer_size = BATCH_SIZE * 4
-
-train_ds = train_locations_ds.map(
-    extract_subvolume_and_label, num_parallel_calls=tf.data.AUTOTUNE
-)
-train_ds = train_ds.prefetch(tf.data.AUTOTUNE).batch(BATCH_SIZE)
-
-
-for subvolume_batch, label_batch in train_ds.take(1):
-    print(f"subvolume shape: {subvolume_batch.shape[1:]}")
-    print(f"label_batch shape: {label_batch.shape[1:]}")
-
-t0 = time.time()
-n = steps_per_epoch // 5
-for subvolume, label in train_ds.take(n):
-    pass
-print(f"Time per batch: {(time.time() - t0) / n:.4f}s")
-
-
-val_locations_stride = BUFFER
-val_locations = []
-for x in range(
-    val_location[0], val_location[0] + val_zone_size[0], val_locations_stride
-):
-    for y in range(
-        val_location[1], val_location[1] + val_zone_size[1], val_locations_stride
-    ):
-        val_locations.append((x, y))
-
-val_locations_ds = tf.data.Dataset.from_tensor_slices(val_locations).filter(
-    is_in_mask_train
-)
-val_ds = val_locations_ds.map(
-    extract_subvolume_and_label, num_parallel_calls=tf.data.AUTOTUNE
-)
-val_ds = val_ds.prefetch(tf.data.AUTOTUNE).batch(BATCH_SIZE)
-
-
-score = trivial_baseline(val_ds).numpy()
-print(
-    f"Best validation score achievable trivially: {score * 100:.2f}% accuracy"
-)  # NANs here?
-
-for subvolume, label in val_ds.take(1):
-    print("VAL_DS LOOKS LIKE:", subvolume.shape, label.shape)
-
-
-# THIS IS CREATING NANs
 augmenter = keras.Sequential(
     [
         keras.layers.RandomContrast(0.2),
     ]
 )
 
+def augment_train_data(data, label):
+    data = tf.cast(augmenter(tf.cast(data, dtype="float32")), dtype="float32")
+    return data, label
 
-augmented_train_ds = train_ds.map(
-    augment_train_data, num_parallel_calls=tf.data.AUTOTUNE
-).prefetch(tf.data.AUTOTUNE)
+def main():
+    mask = load_mask(split="train", index=1)
+    labels = load_labels(split="train", index=1)
 
-for subvolume, label in train_ds.take(1):
-    print("NANs in train subvolume?", tf.math.reduce_any(tf.math.is_nan(subvolume)))
-for subvolume, label in augmented_train_ds.take(1):
-    print("NANs in augmented subvolume?", tf.math.reduce_any(tf.math.is_nan(subvolume)))
+    mask_test_a = load_mask(split="test", index="a")
+    mask_test_b = load_mask(split="test", index="b")
 
-del train_ds
+    mask_train_1 = load_mask(split="train", index=1)
+    labels_train_1 = load_labels(split="train", index=1)
 
-# important note: the original model is ~2 million params!
-# it also appears to be using an addition-based residual u-net. I wonder if
-# this can be done better using concatenation/densenets, either with conv
-# layers or perhaps with self-attention? This is where I want to experiment!
-#
-# new model will be located in architecture.py!
+    mask_train_2 = load_mask(split="train", index=2)
+    labels_train_2 = load_labels(split="train", index=2)
 
-del volume
-del mask
-del labels
+    mask_train_3 = load_mask(split="train", index=3)
+    labels_train_3 = load_labels(split="train", index=3)
 
-model = get_model((BUFFER * 2, BUFFER * 2, Z_DIM))
-model.summary()
-model.compile(
-    # optimizer="adam",
-    optimizer=tf.keras.optimizers.Adam(learning_rate=learnRate, beta_1=momentum),
-    # loss="binary_crossentropy",
-    loss=tf.keras.losses.BinaryCrossentropy(),
-    metrics=["accuracy"],
-    jit_compile=USE_JIT_COMPILE,
-    run_eagerly=True,
-)
+    gc.collect()
 
-# for no reason at all, I'll note that this is the last line in monsieur
-# Chollet's training regime:
-# loss: 0.1243 - accuracy: 0.9513 - val_loss: 0.8680 - val_accuracy: 0.7115
-# consider this the number to beat :)
-#
-# update: this was my 4 million param CNN's result:
-# loss: 0.0544 - accuracy: 0.9784 - val_loss: 0.1387 - val_accuracy: 0.9516
-# eat your heart out, Francois!
+    volume = load_volume(split="train", index=1)
+    gc.collect()
+    volume = tf.concat([volume, load_volume(split="train", index=2)], axis=1)
+    gc.collect()
+    volume = tf.concat([volume, load_volume(split="train", index=3)], axis=1)
+    # so what has happened now is that we have a 2D image containing our ENTIRE
+    # train set, shaped [4000,8417,20] (that is, a 2D image 20 layers deep!), which
+    # contains pages 1, 2, and 3. Similarly, our labels and mask (set below) will
+    # contain all three pages as well!
+    # note that the size listed above depends on SHARED_HEIGHT, which I might mess
+    # with because my computer is getting OOM errors
+    print(f"total volume: {volume.shape}, {volume.dtype}")
+    print(
+        f"Volume's max: {tf.math.reduce_max(volume)}, min: {tf.math.reduce_min(volume)},mean: {tf.math.reduce_mean(volume)}\n"
+    )
+
+    gc.collect()
+
+    labels = tf.concat([labels_train_1, labels_train_2, labels_train_3], axis=1)
+    print(f"labels: {labels.shape}, {labels.dtype}")
+
+    mask = tf.concat([mask_train_1, mask_train_2, mask_train_3], axis=1)
+    print(f"mask: {mask.shape}, {mask.dtype}")
+
+    # Free up memory
+    del labels_train_1
+    del labels_train_2
+    del labels_train_3
+    del mask_train_1
+    del mask_train_2
+    del mask_train_3
+
+    # val_location = (1300, 1000)
+    # val_zone_size = (600, 2000)
+    # gotta change these to match our variable height:
+    val_location = (
+        int((1300 / 4000) * SHARED_HEIGHT),
+        int((1300 / 8417) * volume.shape[1]),
+    )
+    val_zone_size = (
+        int((600 / 4000) * SHARED_HEIGHT),
+        int((2000 / 8417) * volume.shape[1]),
+    )
+    print(val_location, val_zone_size)
 
 
-class EveryKCallback(keras.callbacks.Callback):
-    def __init__(self, epoch_interval=epoch_interval):
-        self.epoch_interval = epoch_interval
+    sample_random_location_train = lambda x: sample_random_location(mask.shape)
+    is_in_mask_train = lambda x: is_in_masked_zone(x, mask)
 
-    def on_epoch_begin(self, epoch, logs=None):
-        if (epoch % self.epoch_interval) == 0:
-            self.model.save_weights(
-                "ckpts/ckpt" + str(epoch), overwrite=True, save_format="h5"
-            )
-            # self.model.save('network',overwrite=True)
+    def is_proper_train_location(location):
+        return not (
+            is_in_val_zone(location, val_location=val_location, val_zone_size=val_zone_size)
+            and is_in_mask_train(location)
+        )
 
 
-model.fit(
-    augmented_train_ds,
-    validation_data=val_ds,
-    epochs=epochs,
-    callbacks=[EveryKCallback()],  # custom callbacks here!
-    # steps_per_epoch=steps_per_epoch
-)
-model.save("model.keras")
+    train_locations_ds = (
+        tf.data.Dataset.from_tensor_slices([0])
+        .repeat(steps_per_epoch)
+        .map(sample_random_location_train, num_parallel_calls=tf.data.AUTOTUNE)
+    )
+    train_locations_ds = train_locations_ds.filter(is_proper_train_location)
 
-del augmented_train_ds
-del val_ds
 
-# Manually trigger garbage collection
-keras.backend.clear_session()
-gc.collect()
+    shuffle_buffer_size = BATCH_SIZE * 4
+
+    def extract_subvolume_and_label(location):
+        subvolume = extract_subvolume(location, volume)
+        label = extract_labels(location, labels)
+        return subvolume, label
+
+    train_ds = train_locations_ds.map(
+        extract_subvolume_and_label, num_parallel_calls=tf.data.AUTOTUNE
+    )
+    train_ds = train_ds.prefetch(tf.data.AUTOTUNE).batch(BATCH_SIZE)
+
+
+    for subvolume_batch, label_batch in train_ds.take(1):
+        print(f"subvolume shape: {subvolume_batch.shape[1:]}")
+        print(f"label_batch shape: {label_batch.shape[1:]}")
+
+    t0 = time.time()
+    n = steps_per_epoch // 5
+    for subvolume, label in train_ds.take(n):
+        pass
+    print(f"Time per batch: {(time.time() - t0) / n:.4f}s")
+
+
+    val_locations_stride = BUFFER
+    val_locations = []
+    for x in range(
+        val_location[0], val_location[0] + val_zone_size[0], val_locations_stride
+    ):
+        for y in range(
+            val_location[1], val_location[1] + val_zone_size[1], val_locations_stride
+        ):
+            val_locations.append((x, y))
+
+    val_locations_ds = tf.data.Dataset.from_tensor_slices(val_locations).filter(
+        is_in_mask_train
+    )
+    val_ds = val_locations_ds.map(
+        extract_subvolume_and_label, num_parallel_calls=tf.data.AUTOTUNE
+    )
+    val_ds = val_ds.prefetch(tf.data.AUTOTUNE).batch(BATCH_SIZE)
+
+
+    score = trivial_baseline(val_ds).numpy()
+    print(
+        f"Best validation score achievable trivially: {score * 100:.2f}% accuracy"
+    )  # NANs here?
+
+    for subvolume, label in val_ds.take(1):
+        print("VAL_DS LOOKS LIKE:", subvolume.shape, label.shape)
+
+
+    augmented_train_ds = train_ds.map(
+        augment_train_data, num_parallel_calls=tf.data.AUTOTUNE
+    ).prefetch(tf.data.AUTOTUNE)
+
+    for subvolume, label in train_ds.take(1):
+        print("NANs in train subvolume?", tf.math.reduce_any(tf.math.is_nan(subvolume)))
+    for subvolume, label in augmented_train_ds.take(1):
+        print("NANs in augmented subvolume?", tf.math.reduce_any(tf.math.is_nan(subvolume)))
+
+    del train_ds
+
+    # important note: the original model is ~2 million params!
+    # it also appears to be using an addition-based residual u-net. I wonder if
+    # this can be done better using concatenation/densenets, either with conv
+    # layers or perhaps with self-attention? This is where I want to experiment!
+    #
+    # new model will be located in architecture.py!
+
+    del volume
+    del mask
+    del labels
+
+    model = get_model((BUFFER * 2, BUFFER * 2, Z_DIM))
+    model.summary()
+    model.compile(
+        # optimizer="adam",
+        optimizer=tf.keras.optimizers.Adam(learning_rate=learnRate, beta_1=momentum),
+        # loss="binary_crossentropy",
+        loss=tf.keras.losses.BinaryCrossentropy(),
+        metrics=["accuracy"],
+        jit_compile=USE_JIT_COMPILE,
+        run_eagerly=True,
+    )
+
+    # for no reason at all, I'll note that this is the last line in monsieur
+    # Chollet's training regime:
+    # loss: 0.1243 - accuracy: 0.9513 - val_loss: 0.8680 - val_accuracy: 0.7115
+    # consider this the number to beat :)
+    #
+    # update: this was my 4 million param CNN's result:
+    # loss: 0.0544 - accuracy: 0.9784 - val_loss: 0.1387 - val_accuracy: 0.9516
+    # eat your heart out, Francois!
+
+
+    class EveryKCallback(keras.callbacks.Callback):
+        def __init__(self, epoch_interval=epoch_interval):
+            self.epoch_interval = epoch_interval
+
+        def on_epoch_begin(self, epoch, logs=None):
+            if (epoch % self.epoch_interval) == 0:
+                self.model.save_weights(
+                    "ckpts/ckpt" + str(epoch), overwrite=True, save_format="h5"
+                )
+                # self.model.save('network',overwrite=True)
+
+
+    model.fit(
+        augmented_train_ds,
+        validation_data=val_ds,
+        epochs=epochs,
+        callbacks=[EveryKCallback()],  # custom callbacks here!
+        # steps_per_epoch=steps_per_epoch
+    )
+    model.save("model.keras")
+
+    del augmented_train_ds
+    del val_ds
+
+    # Manually trigger garbage collection
+    keras.backend.clear_session()
+    gc.collect()
+
+if __name__=="__main__":
+    main()
